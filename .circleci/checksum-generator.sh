dd1700d87ed06cdd876d3990c8bc286501a3a489
Marcos Pereira <marcos.silva@gmail.com>
2024-09-18 14:49:04 -0400

    circleci: remove duplicated info from checksum generator output

--- .circleci/checksum-generator.sh
+++ .circleci/checksum-generator.sh
@@ -3,7 +3,6 @@
 CHECKSUM_FILE="/tmp/checksum.txt"
 
 find . -name "pom.xml" -print0 | while read -d $'\0' file; do
-    echo "Adding $file to list of checksums"
     echo $(openssl dgst -sha256 "$file") >> "$CHECKSUM_FILE"
 done
 
